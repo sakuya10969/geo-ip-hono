1082d33b963af0cd942429d603c0dc7d4c9c5dbf
sakuya10969 <sakuya.f20010618@gmail.com>
2025-09-30 17:20:02 +0900

    refactor: utils

--- src/utils/ip-validator.ts
+++ src/utils/ip-validator.ts
@@ -1,4 +1,4 @@
-export function isValidIp(ip: string): boolean {
+export const isValidIp = (ip: string): boolean => {
     const ipv4FourDigitsRegex = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/;
     const match = ip.match(ipv4FourDigitsRegex);
   
@@ -7,7 +7,7 @@ export function isValidIp(ip: string): boolean {
     // 各桁が0～255の範囲内かチェック
     return match.slice(1).every((num) => {
       const n = parseInt(num, 10);
-      return n >= 0 && n <= 255;
-    });
-  }
+    return n >= 0 && n <= 255;
+  });
+};
   
